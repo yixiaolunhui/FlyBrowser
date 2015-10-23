becbd8f446821f9caf0aeeabebef399db9d833fb
xufeifandj <459821731@qq.com>
2015-10-23 17:17:40 +0800

    Initial commit by ferris email:459821731 2015年10月23日 17:17:18

---
+++ app/src/main/java/www/flybrowser/net/flybrowser/BrowserActivity.java
@@ -0,0 +1,15 @@
+package www.flybrowser.net.flybrowser;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2015/10/23.
+ */
+public class BrowserActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
